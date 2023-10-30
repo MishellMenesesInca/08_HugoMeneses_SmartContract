759753b9ffe6b4ffee1b8e5aaf873a9d3679c326
MishellMenesesInca <89934483+MishellMenesesInca@users.noreply.github.com>
2023-10-30 17:39:42 -0500

    Update BilleteraDigital.sol

--- T01_HugoMeneses-SmartContrat-Wallet/contracts/BilleteraDigital.sol
+++ T01_HugoMeneses-SmartContrat-Wallet/contracts/BilleteraDigital.sol
@@ -2,38 +2,40 @@
 
 pragma solidity ^0.8.13;
 
-//Billetera de ether
-contract BilleteraDigital{
+/// @title Billetera Digital - Un contrato inteligente para gestionar una billetera de ether.
+/// @dev Este contrato permite depositar, enviar y consultar el saldo de ether en la billetera.
+contract BilleteraDigital {
+    // Dirección del propietario de la billetera.
+    address public owner;
 
-          //Dirección del propietario
-          address public owner;
+    /// @dev Constructor del contrato que establece al propietario inicial.
+    /// @param _owner La dirección del propietario de la billetera.
+    constructor(address _owner) {
+        owner = _owner;
+    }
 
-          //Constructor del smart contract
-          constructor(address _owner) {
-              owner = _owner;
-          }
+    /// @notice Permite depositar ether en la billetera.
+    /// @dev Esta función acepta ether y lo deposita en la billetera.
+    function deposit() payable public {
+        // No se requiere lógica adicional para el depósito.
+    }  
 
-          //Función depositar dinero
-          function deposit() payable public{
-          }  
+    /// @notice Permite enviar ether a otra dirección.
+    /// @dev Solo el propietario puede enviar ether a una dirección específica.
+    /// @param to La dirección a la que se enviará el ether.
+    /// @param amount La cantidad de ether a enviar.
+    function send(address payable to, uint amount) public {
+        if (msg.sender == owner) {
+            to.transfer(amount);
+            return;
+        }
+        revert("Esta cuenta no puede enviar dinero");
+    }
 
-
-          //Función enviar dinero
-          function send(address payable to, uint amount)public{
-            if(msg.sender==owner){
-              to.transfer(amount);
-              return;
-            }
-            revert("Esta cuenta no puede enviar dinero");
-          }
-
-          //Función obtener el balance de la billetera
-          function balanceOf() view public returns(uint){
-              return address(this).balance;
-          }
-
-
-
-
-
+    /// @notice Obtiene el saldo actual de la billetera.
+    /// @dev Esta función consulta el saldo de ether en la billetera.
+    /// @return El saldo actual de ether en la billetera.
+    function balanceOf() view public returns(uint) {
+        return address(this).balance;
+    }
 }
